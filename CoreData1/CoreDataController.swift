5f5e44ed41c590d2ccb0ddd35bcb897ecea6e139
Ed Mosher <ed@eds-macbook-pro.home>
2017-02-07 10:33:55 -0500

    Added CoreDataController.swift and code from AppDelegate.swift and added tableView to the Main.storyboard. Added the ToDoEntity and its one attribute: name.

---
+++ CoreData1/CoreDataController.swift
@@ -0,0 +1,51 @@
+//
+//  CoreDataController.swift
+//  CoreData1
+//
+//  Created by Ed Mosher on 2/7/17.
+//  Copyright © 2017 Ed Mosher. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+  
+  // Not sure why this was added
+  private init() {
+  }
+  
+  // Set up the Managed Object Context
+  class func getContext() -> NSManagedObjectContext {
+    return CoreDataManager.persistentContainer.viewContext
+  }
+  
+  // MARK: - Core Data stack
+  static var persistentContainer: NSPersistentContainer = {
+    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
+    let container = NSPersistentContainer(name: "CoreData1")
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    return container
+  }()
+  
+  // MARK: - Core Data Saving support
+  class func saveContext () {
+    let context = persistentContainer.viewContext
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
+    }
+  }
+  
+  
+  
+}
+
--- CoreData1/ViewController.swift
+++ CoreData1/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+  
+  //var toDos: [NSManagedObject] = []
+  var toDos = [NSManagedObject]()
+  
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    //insertData()
+    //fetchData()
+    
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(true)
+    
+    //let fetchRequest: NSFetchRequest<ToDo> = NSFetchRequest(entityName: "ToDo")
+    let fetchRequest: NSFetchRequest<ToDo> = ToDo.fetchRequest()
+    do {
+      toDos = try CoreDataManager.getContext().fetch(fetchRequest)
+    } catch {
+      print("Could not fetch. \(error)")
+      //print("Error: \(error)")
+    }
+    
+    
   }
-
-
+  
+  // Insert the data (This was used to initially populate the data model)
+  func insertData() {
+    let toDo: ToDo = NSEntityDescription.insertNewObject(forEntityName: "ToDo", into: CoreDataManager.getContext()) as! ToDo
+    toDo.name = "To-do1"
+    CoreDataManager.saveContext()
+  }
+  
+  // Fetch the data (This was used to initially display the data)
+  func fetchData() {
+    let fetchRequest: NSFetchRequest<ToDo> = ToDo.fetchRequest()
+    do {
+      let searchResults = try CoreDataManager.getContext().fetch(fetchRequest)
+      print("Number of results: \(searchResults.count)")
+      for result in searchResults {
+        print("To-do: \(result.name)")
+      }
+    } catch {
+      print("Error: \(error)")
+    }
+  }
+  
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return toDos.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let item = toDos[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = item.value(forKeyPath: "name") as? String
+    return cell
+  }
+}
